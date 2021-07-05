1250b6bd8df455325cf7570dcdb78a1226dcfb97
Esteban Canteros <estebanacanteros@gmail.com>
2021-07-05 11:42:14 -0300

    first commit & starting with tasks module

---
+++ src/tasks/task.model.ts
@@ -0,0 +1,12 @@
+export interface Task {
+  Id: string;
+  title: string;
+  description: string;
+  status: TaskStatus;
+}
+
+enum TaskStatus {
+  OPEN = 'OPEN',
+  IN_PROGRESS = 'IN_PROGRESS',
+  DONE = 'DONE'
+}
