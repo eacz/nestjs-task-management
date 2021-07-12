01b4610fd35729b175be3b6eecbce376cce1313a
Esteban Canteros <estebanacanteros@gmail.com>
2021-07-12 20:35:44 -0300

    validation on filter and search tasks

--- src/tasks/dto/get-tasks-filter.dto.ts
+++ src/tasks/dto/get-tasks-filter.dto.ts
@@ -1,6 +1,12 @@
+import { IsEnum, IsNotEmpty, IsOptional, IsString } from "class-validator";
 import { TaskStatus } from "../task.model";
 
 export class getTasksFilterDto {
+  @IsOptional()
+  @IsEnum(TaskStatus, {message: "The task status should be 'OPEN', 'DONE' or 'IN_PROGRESS'", })
   status?: TaskStatus;
-  search?: string;
+
+  @IsOptional()
+  @IsString()
+  search: string;
 }
