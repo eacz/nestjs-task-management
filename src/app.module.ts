b515caeaa619707f6b202b7c78e2cf3cf6f23a92
Esteban Canteros <estebanacanteros@gmail.com>
2021-08-14 23:45:40 -0300

    ready for deploy

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,33 +12,28 @@ import { configValidationSchema } from './config.schema';
       validationSchema: configValidationSchema
     }),
     TasksModule,
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.DATABASE_HOST,
-      port: Number(process.env.DATABASE_PORT),
-      username: process.env.DATABASE_USERNAME,
-      password: process.env.DATABASE_PASSWORD,
-      database: process.env.DATABASE_NAME,
-      autoLoadEntities: true,
-      synchronize: true,
-    }),
     //load a module asynchronously
-    //TypeOrmModule.forRootAsync({
-    //  imports: [ConfigModule],
-    //  inject: [ConfigService],
-    //  useFactory: async (configService: ConfigService) => {
-    //    return {
-    //      type: 'postgres',
-    //      host: configService.get('DATABASE_HOST'),
-    //      port: configService.get('DATABASE_PORT'),
-    //      username: configService.get('DATABASE_USERNAME'),
-    //      password: configService.get('DATABASE_PASSWORD'),
-    //      database: configService.get('DATABASE_NAME'),
-    //      autoLoadEntities: true,
-    //      synchronize: true,
-    //    }
-    //  }
-    //}),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => {
+        const isProduction = configService.get('STAGE') === 'prod';
+        return {
+          ssl: isProduction,
+          extra: {
+            ssl: isProduction ? { rejectUnauthorized: false } : null
+          },
+          type: 'postgres',
+          host: configService.get('DATABASE_HOST'),
+          port: configService.get('DATABASE_PORT'),
+          username: configService.get('DATABASE_USERNAME'),
+          password: configService.get('DATABASE_PASSWORD'),
+          database: configService.get('DATABASE_NAME'),
+          autoLoadEntities: true,
+          synchronize: true,
+        }
+      }
+    }),
     AuthModule,
   ],
 })
